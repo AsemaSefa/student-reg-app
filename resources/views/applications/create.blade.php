e5e28ad1e171c019625449d2d914e9cc03e28d2a
Asema sefa <sefaasema2@gmail.com>
2024-11-01 09:05:22 +0100

    created a layout component and also setup the index template

--- resources/views/applications/create.blade.php
+++ resources/views/applications/create.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
+<x-layout>
     <div class="container mt-5">
         <h2>Registration Form</h2>
         <form action={{route('applications.store')}} method="POST" enctype="multipart/form-data">
@@ -232,7 +224,6 @@
         </form>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+
+</x-layout>
+   
