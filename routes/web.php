21abe5d4b65d5497feb15824d4acea9a82745bd8
MfenaKaase <68476762+MfenaKaase@users.noreply.github.com>
2024-11-11 08:51:41 +0100

    added login form and authentication logic

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::post('login', [LoginController::class, 'authenticate'])->name('authenticate');
 
 Route::resource('applications', ApplicationController::class)->names('applications');
